4b2bdf96dc9a181f71048fb3386037e74dc72deb
miguel <miguelmartinsambrosio@gmail.com>
2022-11-09 08:37:12 -0500

    requisito 01

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        afg
+      </div>
+    );
+  }
+}
+export default Login;
