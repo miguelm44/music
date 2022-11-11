586d55d72d7ea5a8d7d3945c0ab795cd340ea6d6
miguel <miguelmartinsambrosio@gmail.com>
2022-11-11 10:11:33 -0500

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,54 @@ import React from 'react';
 import Header from '../componets/Header';
 
 class Search extends React.Component {
+  state = {
+    habilitar: true,
+    nomeArtista: '',
+  };
+
+  nome = ({ name, value }) => {
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.habilitarBtn();
+    });
+  };
+
+  habilitarBtn = () => {
+    const { nomeArtista } = this.state;
+    if (nomeArtista.length >= 2) {
+      this.setState({
+        habilitar: false,
+      });
+    } else {
+      this.setState({
+        habilitar: true,
+      });
+    }
+  };
+
   render() {
+    const { habilitar, nomeArtista } = this.state;
     return (
       <div data-testid="page-search">
-        oi
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ ({ target }) => this.nome(target) }
+            name="nomeArtista"
+            value={ nomeArtista }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            name="button"
+            disabled={ habilitar }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
